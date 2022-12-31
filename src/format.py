c05fdae583474b3e3b9e2d495849b01f7170b2d8
ZPB <3295426768@qq.com>
2022-12-31 17:51:02 +0800

    short

--- src/format.py
+++ src/format.py
@@ -1,8 +1,9 @@
+
 def func(text1, maxlength):
     indention = []
     style = []
     space = []
-    
+    global t
     try:
         if text1[1][0:4] == '    ':
             for t in range(0, len(text1)):
@@ -12,7 +13,7 @@ def func(text1, maxlength):
                         # 0:'├─' , 1:'└─', 2:''
                         style.append(0)
                         text1[t] = text1[t][4*i:]
-                        space.append(['    ']*i)
+                        space.append(['  ']*i)
                         break
                     elif text1[t][0] != ' ':  # the first level can not indent
                         indention.append(0)
@@ -28,7 +29,7 @@ def func(text1, maxlength):
                         # 0:'├─' , 1:'└─', 2:''
                         style.append(0)
                         text1[t] = text1[t][i:]
-                        space.append(['    ']*i)
+                        space.append(['  ']*i)
                         break
                     elif t == 0:  # the first level can not indent
                         indention.append(0)
@@ -37,9 +38,8 @@ def func(text1, maxlength):
                         space.append('')
                         break
     except:
-        print('第',t+1,'行出错了！！')
-        sleep(3)
-        exit()
+        ep = '第'+str(t+1)+'行出错了！！'
+        return ep
     #  print(indention)
     print('analyzing...')
     for i in range(1, len(text1)):
@@ -55,15 +55,15 @@ def func(text1, maxlength):
                 break
         for k in range(i+1, j):
             if style[i] == 0:
-                space[k][indention[i]-1] = '  │ '
+                space[k][indention[i]-1] = ' │'
         match style[i]:
             case 0:
-                space[i][indention[i]-1] = '  ├─'
+                space[i][indention[i]-1] = ' ├'
             case 1:
-                space[i][indention[i]-1] = '  └─'
+                space[i][indention[i]-1] = ' └'
         #  print(i, j, indention[i])
     style[len(text1)-1] = 1
-    space[len(text1)-1][indention[len(text1)-1]-1] = '  └─'
+    space[len(text1)-1][indention[len(text1)-1]-1] = ' └'
     #  print(style)
     pstr = ''
     sstr = ''
@@ -76,18 +76,18 @@ def func(text1, maxlength):
             sstr += pstr + text1[i][:maxlength - len(pstr)] + '\n'
             text1[i] = text1[i][maxlength - len(pstr):]
             if style[i] == 0:
-                replace = '  │ '
+                replace = ' │'
             else:
-                replace = '    '
+                replace = '  '
             while len(pstr + text1[i] + '\n') >= maxlength:
-                sstr += pstr[:-4]+ replace + text1[i][:maxlength - len(pstr)] + '\n'
+                sstr += pstr[:-2]+ replace + text1[i][:maxlength - len(pstr)] + '\n'
                 text1[i] = text1[i][maxlength - len(pstr):]
-            sstr += pstr[:-4]+ replace + text1[i] + '\n'
+            sstr += pstr[:-2]+ replace + text1[i] + '\n'
             pstr = ''
             continue
         #  print(pstr + text1[i])
         if text1[0] == '@@':
-            pstr = pstr[4:]
+            pstr = pstr[2:]
             if i == 0:
                 continue
         sstr += pstr + text1[i] + '\n'
