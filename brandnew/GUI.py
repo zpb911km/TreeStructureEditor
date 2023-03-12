002d05e1a8d381aa293ebbb0d1459f3d26a9883c
ZPB <3295426768@qq.com>
2023-03-12 14:14:22 +0800

    instant newcore

---
+++ brandnew/GUI.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitled.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
+    QMainWindow, QMenuBar, QPlainTextEdit, QPushButton,
+    QSizePolicy, QSlider, QStatusBar, QVBoxLayout,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(798, 577)
+        icon = QIcon()
+        icon.addFile(u"\"E:/myfiles/python/symbol.png\"", QSize(), QIcon.Normal, QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setMaximumSize(QSize(67, 16777215))
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.Slider = QSlider(self.centralwidget)
+        self.Slider.setObjectName(u"Slider")
+        self.Slider.setMaximum(200)
+        self.Slider.setOrientation(Qt.Horizontal)
+
+        self.horizontalLayout_2.addWidget(self.Slider)
+
+        self.maxlength = QLabel(self.centralwidget)
+        self.maxlength.setObjectName(u"maxlength")
+
+        self.horizontalLayout_2.addWidget(self.maxlength)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        font = QFont()
+        font.setFamilies([u"Cascadia Mono"])
+        font.setPointSize(12)
+        font.setBold(False)
+        self.plainTextEdit.setFont(font)
+
+        self.verticalLayout.addWidget(self.plainTextEdit)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+        self.Open = QPushButton(self.centralwidget)
+        self.Open.setObjectName(u"Open")
+
+        self.horizontalLayout.addWidget(self.Open)
+
+        self.Save = QPushButton(self.centralwidget)
+        self.Save.setObjectName(u"Save")
+
+        self.horizontalLayout.addWidget(self.Save)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 798, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"format", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u6700\u5927\u957f\u5ea6:", None))
+        self.maxlength.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.plainTextEdit.setPlainText("")
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u63d0\u793a\uff1a", None))
+        self.Open.setText(QCoreApplication.translate("MainWindow", u"\u6253\u5f00", None))
+        self.Save.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58", None))
+    # retranslateUi
+
---
+++ brandnew/display.py
@@ -0,0 +1,77 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPlainTextEdit, QFileDialog
+from PySide6.QtGui import QTextCursor
+from GUI import Ui_MainWindow
+from newcore import format
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.Slider.setValue(200)
+        self.ui.maxlength.setText('200')
+        # 禁止自动换行
+        self.ui.plainTextEdit.setLineWrapMode(QPlainTextEdit.NoWrap)
+        # self.ui.plainTextEdit.blockCountChanged.connect(self.format)
+        self.ui.plainTextEdit.textChanged.connect(self.format)
+        self.ui.Slider.valueChanged.connect(self.getchange)
+        self.ui.Slider.valueChanged.connect(self.format)
+        self.ui.Open.clicked.connect(self.Open)
+        self.ui.Save.clicked.connect(self.Save)
+
+    def getchange(self):
+        self.ui.maxlength.setText(str(self.ui.Slider.value()))
+
+    def format(self):
+        text = self.ui.plainTextEdit.toPlainText()
+        try:
+            DoneText = format(text, self.ui.Slider.value())
+            Cursor = self.ui.plainTextEdit.textCursor()
+            P = Cursor.position()  # 获取光标位置
+            self.ui.plainTextEdit.textChanged.disconnect(self.format)
+            self.ui.plainTextEdit.setPlainText(DoneText)
+            self.ui.plainTextEdit.textChanged.connect(self.format)
+            Cursor.setPosition(P)  # 光标归位
+            #  Cursor.movePosition(QTextCursor.End)
+            self.ui.plainTextEdit.setTextCursor(Cursor)
+            self.ui.lineEdit.setText('')
+        except Exception as E:
+            self.ui.lineEdit.setText(str(E))
+    
+    def Open(self):
+        PATH, _ = QFileDialog.getOpenFileName(
+            self,
+            '打开',
+            "E:\myfiles\总结梳理",
+            "文本文件 (*.zpb *.txt)")
+        try:
+            file = open(PATH, encoding='UTF-8')
+            text = file.read()
+        except UnicodeDecodeError:
+            file = open(PATH, encoding='ANSI')
+            text = file.read()
+        file.close()
+        self.ui.plainTextEdit.textChanged.disconnect(self.format)
+        self.ui.plainTextEdit.setPlainText(format(text))
+        self.ui.plainTextEdit.textChanged.connect(self.format)
+
+    def Save(self):
+        text = self.ui.plainTextEdit.toPlainText()
+        try:
+            PATH, _ = QFileDialog.getSaveFileName(
+                self,
+                "保存",
+                "E:\\myfiles\\总结梳理",
+                "文本文件 (*.zpb)")
+            with open(PATH, 'w', encoding='UTF-8') as file:
+                file.write(format(text, 200))
+        except Exception as E:
+            self.ui.lineEdit.setText(str(E) + ' !Save Error!')
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    main_window = MainWindow()
+    main_window.show()
+    app.exec()
